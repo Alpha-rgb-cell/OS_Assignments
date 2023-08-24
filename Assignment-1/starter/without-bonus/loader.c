c3d0a1377fd7ffaf41e160065a3e926f26f3fc7c
Athiyo <76748228+Alpha-rgb-cell@users.noreply.github.com>
2023-08-24 22:08:15 +0530

    Update loader.c

--- Assignment-1/starter/without-bonus/loader.c
+++ Assignment-1/starter/without-bonus/loader.c
@@ -1,42 +1,49 @@
 #include "loader.h"
 #include <elf.h>
 
+// Global variables to hold ELF header, program header, and file descriptor
 Elf32_Ehdr *ehdr;
 Elf32_Phdr *phdr;
 int fd;
 
+// Function to clean up resources
 void loader_cleanup() {
     if (fd != -1) {
-        close(fd);
+        close(fd); // Close the file descriptor if it's open
     }
     if (ehdr != NULL) {
-        free(ehdr);
+        free(ehdr); // Free allocated memory for ELF header
     }
     if (phdr != NULL) {
-        free(phdr);
+        free(phdr); // Free allocated memory for program header
     }
 }
 
+// Function to load and execute an ELF executable
 void load_and_run_elf(char **exe) {
+    // Open the ELF file for reading
     fd = open(exe[1], O_RDONLY);
     if (fd == -1) {
         perror("Error opening file.");
         exit(EXIT_FAILURE);
     }
 
+    // Allocate memory for and read the ELF header
     ehdr = (Elf32_Ehdr *)malloc(sizeof(Elf32_Ehdr));
     read(fd, ehdr, sizeof(Elf32_Ehdr));
 
+    // Allocate memory for and read the program header table
     phdr = (Elf32_Phdr *)malloc(ehdr->e_phentsize * ehdr->e_phnum);
     lseek(fd, ehdr->e_phoff, SEEK_SET);
     read(fd, phdr, ehdr->e_phentsize * ehdr->e_phnum);
 
     int i = 0;
     while (i < ehdr->e_phnum) {
-        if (phdr[i].p_type == PT_LOAD) {
+        if (phdr[i].p_type == PT_LOAD) { // Check for PT_LOAD segment
             if (ehdr->e_entry >= phdr[i].p_vaddr && ehdr->e_entry <= phdr[i].p_vaddr + phdr[i].p_filesz) {
+                // Map virtual memory and load segment content
                 void *virtual_mem = mmap((void *)phdr[i].p_vaddr, phdr[i].p_memsz,
-                                         PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+                PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
                 if (virtual_mem == MAP_FAILED) {
                     perror("Error mapping memory");
                     close(fd);
@@ -44,23 +51,26 @@ void load_and_run_elf(char **exe) {
                     exit(EXIT_FAILURE);
                 }
                 
+                // Read segment content from ELF file and load into memory
                 lseek(fd, phdr[i].p_offset, SEEK_SET);
                 read(fd, virtual_mem, phdr[i].p_filesz);
 
+                // Calculate entry point offset and execute _start function
                 uintptr_t entry_offset = ehdr->e_entry - phdr[i].p_vaddr;
                 char *entry_address = (char *)((uintptr_t)virtual_mem + entry_offset);
                 int (*_start)() = (int (*)())entry_address;
                 int result = _start();
                 printf("User _start return value = %d\n", result);
 
-                // Clean up
+                // Clean up: unmap memory
                 munmap(virtual_mem, phdr[i].p_memsz);
-                break; // Exit the loop after finding the first LOAD segment
+                break; // Exit the loop after the first PT_LOAD segment is found
             }
         }
         i++;
     }
 
+    // Clean up resources
     loader_cleanup();
     close(fd);
     exit(EXIT_FAILURE);
