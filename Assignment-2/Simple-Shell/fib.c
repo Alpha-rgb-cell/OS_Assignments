82e0f96053da77f53fcb17cf883ee72cd71d31d8
Athiyo <76748228+Alpha-rgb-cell@users.noreply.github.com>
2023-09-09 19:54:06 +0530

    Update fib.c

--- Assignment-2/Simple-Shell/fib.c
+++ Assignment-2/Simple-Shell/fib.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int fib(int n) {
-    if (n <= 1) return n;
-    return fib(n - 1) + fib(n - 2);
+// Function to calculate Fibonacci numbers
+int fibonacci(int n) {
+    if (n <= 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
 
-int main() {
-    int n = 10; // Adjust the Fibonacci number you want to calculate
-    printf("Fibonacci(%d) = %d\n", n, fib(n));
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <n>\n", argv[0]);
+        return 1;
+    }
+
+    int n = atoi(argv[1]);
+
+    if (n < 0) {
+        printf("Please provide a non-negative integer.\n");
+        return 1;
+    }
+
+    int result = fibonacci(n);
+    printf("Fibonacci(%d) = %d\n", n, result);
+
     return 0;
 }
